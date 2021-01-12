913241fb49d28c5a78d069028459c8e915d1fe24
Monty <moonvijh89@gmail.com>
2021-01-12 18:35:56 +0530

    Liquid Stacking Contract and Stake Functionality

--- contracts/liquidStaking.sol
+++ contracts/liquidStaking.sol
@@ -1,15 +1,130 @@
 // SPDX-License-Identifier: MIT
-pragma solidity ^0.7.5;
+pragma solidity ^0.6.0;
 
-import "./uToken.sol";
-import "./sToken.sol";
+import "uTokens.sol";
+import "sTokens.sol";
 
-contract liquidStaking {
-    uToken private _uToken;
-    sToken private _sToken;
-
-    function mintUToken(address to, uint256 tokens) public returns (bool success) {
-        _uToken.mint(to, tokens);
-         return true;
+contract liquidStacking {
+    
+    //Event to track the setting of contracts
+    event SetContract(
+        address indexed _contract
+    );
+    
+    //Event to track the minting of tokens
+    event MintTokens(
+        address indexed _from,
+        uint256 _value
+    );
+    
+    //Event to track Staking
+    event Staking(
+        address indexed _from,
+        uint256 _value
+    );
+    
+    uTokens private UTokens;
+    sTokens private STokens;
+    
+    
+    /**
+     * @dev Sets the values for {utoken contract address} and {stoken contract address}
+     * @param _uaddress: utoken address, _saddress: stoken address
+     *
+     * Both contract addresses are immutable: they can only be set once during
+     * construction.
+     */
+    constructor(address _uaddress, address _saddress) public {
+       setUTokensContract(_uaddress);
+       setSTokensContract(_saddress);
     }
-}
+    
+    /** 
+     * @dev Set 'contract address', called from constructor
+     * @param _contract: utoken contract address
+     * 
+     * Emits a {SetContract} event with '_contract' set to the utoken contract address.
+     *
+     */
+    function setUTokensContract(address _contract) private {
+        UTokens = uTokens(_contract);
+        emit SetContract(_contract);
+        
+    }
+    
+    /** 
+     * @dev Set 'contract address', called from constructor
+     * @param _contract: stoken contract address
+     * 
+     * Emits a {SetContract} event with '_contract' set to the stoken contract address.
+     *
+     */
+    function setSTokensContract(address _contract) private {
+        STokens = sTokens(_contract);
+        emit SetContract(_contract);
+    }
+    
+    /** 
+     * @dev Mint new utokens for the provided 'address' and 'amount'
+     * @param to: account address, amount: number of tokens
+     * 
+     * Emits a {MintTokens} event with 'to' set to address and 'amount' set to amount of tokens.
+     * 
+     * Requirements:
+     *
+     * - `amount` cannot be less than zero.
+     *
+     */
+    function generateUTokens(address to, uint256 amount) public {
+        require(amount>0, "Token Amount should be greater than 0");
+        UTokens.mint(to, amount);
+        emit MintTokens(to, amount);
+        
+    }
+    
+    /** 
+     * @dev Get utoken balance for the 'address'
+     * @param _address: utoken contract address
+     *
+     */
+    function getUtokenBalance(address _address) public view returns(uint256) {
+        return UTokens.balanceOf(_address);
+    }
+    
+    /** 
+     * @dev Get utoken balance for the 'address'
+     * @param _address: utoken contract address
+     *
+     */
+    function getStokenBalance(address to) public view returns(uint256) {
+        return STokens.balanceOf(to);
+    }
+    
+    function transferUToken(address from, address to, uint256 amount) public returns (bool) {
+        return UTokens.transferFrom(from, to, amount);
+    }
+    
+    
+    function stake(address to, uint256 utok) public returns(bool) {
+        // Check the supplied amount is greater than 0
+        require(utok>0, "Number of staked tokens should be greater than 0");
+        // Check the current balance for uTokens is greater than the amount to be staked
+        uint256 currentUTokenBalance = UTokens.balanceOf(to);
+        require(currentUTokenBalance>utok, "Insuffcient balance for account");
+        // Burn the uTokens as specified with the amount
+        UTokens.burn(to, utok);
+        // Mint the sTokens for the account specified
+        STokens.mint(to, utok);
+        // Verify the staking
+        uint256 newUTokenBalance = UTokens.balanceOf(to);
+        uint256 verifyBalance = newUTokenBalance + utok;
+        require(currentUTokenBalance == verifyBalance, "Stake Unsuccessful");
+        emit Staking(to, utok);
+        return true;
+        
+        
+    }
+    
+    }
+    
+    
--- contracts/uToken.sol
+++ contracts/uToken.sol
@@ -1,29 +1,21 @@
 // SPDX-License-Identifier: MIT
-pragma solidity ^0.7.5;
+pragma solidity ^0.6.0;
 
-import "https://github.com/OpenZeppelin/openzeppelin-contracts/blob/v3.2.2-solc-0.7/contracts/token/ERC20/ERC20.sol";
+import "https://github.com/OpenZeppelin/openzeppelin-contracts/blob/master/contracts/token/ERC20/ERC20.sol";
 
-contract uToken is ERC20 {
 
-    constructor () ERC20("uAtoms", "XPRT") {
-        _mint(_msgSender(), 0);
+contract uTokens is ERC20 {
+    constructor() public ERC20("uAtoms", "uAtoms") {
+        _mint(msg.sender, 0);
     }
-
-     function mint(address to, uint256 tokens) public returns (bool success) {
-         uint256 token = tokens * (10**uint256(decimals()));
-         _mint(to, token);
-         return true;
-     }
-
-     function burn(address from, uint256 tokens) public returns (bool success) {
-         uint256 token = tokens * (10**uint256(decimals()));
-       _burn(from, token);
-         return true;
-     }
-
-     function transfer(address to, uint256 tokens) public override returns (bool success) {
-         uint256 token = tokens * (10**uint256(decimals()));
-       _transfer(_msgSender(), to, token);
-         return true;
-     }
+    
+    function mint(address to, uint256 amount) public {
+        _mint(to, amount);
+    }
+    
+    function burn(address to, uint256 amount) public {
+        _burn(to, amount);
+    }
+    
+    
 }
