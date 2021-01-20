995a349ed4b173d7da474f351d90e0c058079388
Aditi  Maheshwari <aditimaheshwari121@gmail.com>
2021-01-20 13:10:49 +0530

    Adding Unit Test Cases.

--- test/LiquidStakingTest.js
+++ test/LiquidStakingTest.js
@@ -1,15 +1,15 @@
 //UNIT TEST
 
-/* This unit test uses the OpenZeppelin test environment and OpenZeppelin test helpers, 
+/* This unit test uses the OpenZeppelin test environment and OpenZeppelin test helpers,
 which we will be using for our unit testing. */
 
-/* consider accounts[0] as LiquidStaking contract owner address, accounts[1] as uTokens contract owner address, 
+/* consider accounts[0] as LiquidStaking contract owner address, accounts[1] as uTokens contract owner address,
 and accounts[2] as sTokens contract owner address, rest 6 accounts you can use as legitimate stakers or malicious user address */
 
-/* You will have to create the following 'default test scenarios' which applies for every smart contract function, 
+/* You will have to create the following 'default test scenarios' which applies for every smart contract function,
 then some 'custom test scenarios' that applies for those specific smart contract functions as per their routine:
 @ All contract functions need their 'require statements' tested
-@ All contract functions need their modifiers tested by calling the function using - owner address, staker address, 
+@ All contract functions need their modifiers tested by calling the function using - owner address, staker address,
 and malicious user address
 @ All contract functions need all the events generated tested using expectEvent (example given below)
 @ Apart from all this, every contract function needs to be tested for the result of its routine seperately
@@ -28,95 +28,184 @@ const {
   expectRevert,
   balance,
 } = require("@openzeppelin/test-helpers");
-var { expect } = require("chai");
-const LiquidStaking = contract.fromArtifact("LiquidStaking");
+const { expect } = require("chai");
+const LiquidStaking = contract.fromArtifact("liquidStaking");
+const sTokens = contract.fromArtifact("sTokens");
+const uTokens = contract.fromArtifact("uTokens");
 
-/* each smart contract function will have one 'describe' test function defined , 
+/* each smart contract function will have one 'describe' test function defined ,
 and multiple 'it' test functions which checks multiple test scenario */
 
 // tests pertaining to all Constructor() calls
-describe("## Constructor() when revert cases happen ##", function () {
-  // VARIABLES USED IN TEST
-  var liquidStaking;
-  var _uaddress;
-  var _saddress;
-  it("TEST: Constructor() when _uaddress is address(0): ", async function () {
-    _uaddress = address(0);
-    _saddress = accounts[2];
-    console.log(
-      "_uaddress: ",
-      _uaddress.toString(),
-      " _saddress: ",
-      _saddress.toString()
-    );
-    // DEPLOY CONTRACT
-    expectRevert(
-      LiquidStaking.new(_uaddress, _saddress, { from: accounts[0] }),
-      "constructor#1"
-    );
-    // DEPLOY END
-  }, 200000); // TEST END
+describe("Liquid Staking", function () {
 
-  it("TEST: Constructor() when _saddress is address(0): ", async function () {
-    _saddress = address(0);
-    _uaddress = accounts[1];
-    console.log(
-      "_uaddress: ",
-      _uaddress.toString(),
-      " _saddress: ",
-      _saddress.toString()
-    );
-    // DEPLOY CONTRACT
-    expectRevert(
-      LiquidStaking.new(_uaddress, _saddress, { from: accounts[0] }),
-      "constructor#1"
-    );
-    // DEPLOY END
-  }, 200000); // TEST END
+    it("TEST: Constructor() when _uaddress is address(0): ", async function () {
+        // DEPLOY CONTRACT
+        expectRevert(
+            LiquidStaking.new(accounts[1], accounts[2], {from: accounts[0]}),
+            "revert"
+        );
+        // DEPLOY END
+    }, 200000); // TEST END
+
+    it("TEST: Constructor() when _saddress is address(0): ", async function () {
+        // DEPLOY CONTRACT
+        expectRevert(
+            LiquidStaking.new(accounts[1], accounts[2], {from: accounts[0]}),
+            "revert"
+        );
+        // DEPLOY END
+    }, 200000); // TEST END
+    describe("Staking", function () {
+        let to = accounts[3];
+        let from = accounts[1];
+        let liquidStaking;
+        let amount = new BN(50);
+        let amt = new BN(100);
+        beforeEach(async function () {
+            let utokens = await uTokens.new({ from: from});
+            let stokens = await sTokens.new({ from: from});
+
+            // DEPLOY CONTRACT
+            liquidStaking = await LiquidStaking.new(utokens.address, stokens.address, {
+                from: from,
+            });
+        });
+        it('generate uTokens', async function () {
+            let generateUTokenTxnReceipt = await liquidStaking.generateUTokens(from,amount,{from: from,});
+            let balance = await liquidStaking.getUtokenBalance(from);
+            expect(balance == amount)
+            expectEvent(generateUTokenTxnReceipt, "Transfer", {
+                value: amount,
+            });
+        });
+
+        it('Number of staked tokens should be greater than 0', async function () {
+            let val = new BN(0);
+            expectRevert(liquidStaking.stake(to,val,{from: from,}),"revert");
+        });
+
+        it('Current uToken balance should be greater than staked amount', async function () {
+            let generateUToken = await liquidStaking.generateUTokens(to,amount,{from: from,});
+            expectEvent(generateUToken, "Transfer", {
+                value: amount,
+            });
+            await expectRevert(liquidStaking.stake(to, amt, {from: from,}), "revert");
+        });
+
+        it('Stake', async function () {
+            let generateUToken = await liquidStaking.generateUTokens(to,amt,{from: from,});
+            let stake = await liquidStaking.stake(to,amount,{from: from,});
+            expectEvent(generateUToken, "Transfer", {
+                to:to,
+                value: amt,
+            });
+            expectEvent(stake, "Transfer", {
+                to:to,
+                value: amount,
+            });
+            expectEvent(stake, "Staking", {
+                _from:to,
+                _value: amount,
+            });
+        });
+    })
+
+    describe("UnStaking", function () {
+        let to = accounts[3];
+        let from = accounts[1];
+        let amount = new BN(100);
+        let amt = new BN(50);
+        let liquidStaking;
+        beforeEach(async function () {
+            let utokens = await uTokens.new({ from: from});
+            let stokens = await sTokens.new({ from: from});
+            // DEPLOY CONTRACT
+            liquidStaking = await LiquidStaking.new(utokens.address, stokens.address, {
+                from: from,
+            });
+        });
+
+        it('Number of unstaked tokens should be greater than 0', async function () {
+            let val = new BN(0);
+            await expectRevert(liquidStaking.unStake(to, val, {from: from,}), "revert");
+        });
+
+        it('Current stoken balance should be greater than unstaked amount', async function () {
+            let generateUToken = await liquidStaking.generateUTokens(to,amt,{from: from,});
+            let stake = await liquidStaking.stake(to,amount,{from: from,});
+            expectEvent(generateUToken, "Transfer", {
+                to:to,
+                value: amt,
+            });
+            expectEvent(stake, "Transfer", {
+                to:to,
+                value: amount,
+            });
+            expectEvent(stake, "Staking", {
+                _from:to,
+                _value: amount,
+            });
+            await expectRevert(liquidStaking.unStake(to, amount, {from: from,}), "revert");
+        });
+
+        it('UnStake', async function () {
+           let generateUToken = await liquidStaking.generateUTokens(to,amount,{from: from,});
+            let stake = await liquidStaking.stake(to,amt,{from: from,});
+            expectEvent(generateUToken, "Transfer", {
+                to:to,
+                value: amount,
+            });
+            expectEvent(stake, "Transfer", {
+                to:to,
+                value: amt,
+            });
+            expectEvent(stake, "Staking", {
+                _from:to,
+                _value: amt,
+            });
+            let unStake = await liquidStaking.unStake(to,amt,{from: from,});
+            expectEvent(unStake, "Transfer", {
+                to:to,
+                value: amt,
+            });
+            expectEvent(unStake, "Unstaking", {
+                _from:to,
+                _value: amt,
+            });
+        });
+
+        it('Withdraw UnStake token before locking period', async function () {
+            let generateUToken = await liquidStaking.generateUTokens(to,amount,{from: from,});
+            let stake = await liquidStaking.stake(to,amt,{from: from,});
+            expectEvent(generateUToken, "Transfer", {
+                to:to,
+                value: amount,
+            });
+            expectEvent(stake, "Transfer", {
+                to:to,
+                value: amt,
+            });
+            expectEvent(stake, "Staking", {
+                _from:to,
+                _value: amt,
+            });
+            let unStake = await liquidStaking.unStake(to,amount,{from: from,});
+            expectEvent(unStake, "Transfer", {
+                to:to,
+                value: amt,
+            });
+            expectEvent(stake, "Unstaking", {
+                _from:to,
+                _value: amt,
+            });
+            await expectRevert(liquidStaking.withdrawUnstakedTokens({from: from,}), "revert");
+        });
+    })
 }); // DESCRIBE END
 
-// tests pertaining to all generateUTokens() calls
-describe("## generateUTokens() txn function related tests ##", function () {
-  // VARIABLES USED IN TEST
-  var liquidStaking;
-  var to;
-  var amount;
-  var _uaddress;
-  var _saddress;
-
-  it("TEST: generateUTokens() successful execution scenario: ", async function () {
-    _saddress = accounts[2];
-    _uaddress = accounts[1];
-    to = accounts[3];
-    amount = new BN("10000000000000000000");
-    // DEPLOY CONTRACT
-    liquidStaking = await LiquidStaking.new(_uaddress, _saddress, {
-      from: accounts[0],
-    });
-    console.log("contract deployed successfully ");
-    // DEPLOY END
-
-    // TEST SCENARIO
-    let generateUTokensTxnReceipt = await liquidStaking.generateUTokens(
-      to,
-      amount,
-      {
-        from: accounts[0],
-      }
-    );
-
-    // test if the event 'Transfer(sender, recipient, amount)' is emitted:
-    expectEvent(generateUTokensTxnReceipt, "Transfer", {
-      sender: constants.ZERO_ADDRESS,
-      recipient: accounts[3],
-      amount: new BN("10000000000000000000"),
-    });
-    // TEST SCENARIO END
-  }, 200000); // TEST END
-}); // DESCRIBE END
-
-/* 
-Other expect statements you can use to verify test scenarios: 
+/*
+Other expect statements you can use to verify test scenarios:
   expect(someBigNumberVal).to.be.bignumber.equal(anotherBigNumberVal);
   expect(someBigNumberVal).to.be.bignumber.equal(new BN(-1));
   expect(someValue).to.not.equal(anotherValue);
