006c0cf929f2b9d17ea8dce7cf4768a2a8af3e56
aditim121 <aditimaheshwari121@gmail.com>
2021-05-18 16:37:43 +0530

    test scripts

---
+++ migrations/pBridgeTestData.js
@@ -0,0 +1,177 @@
+module.exports = {
+    pBridgeTestData : [
+        {
+            action: "stake",
+            user: "0x609d344A04245104C312925D2F5aE04F643A10CB",
+            amount: "8",
+        },
+        {
+            action: "stake",
+            user: "0x7019943Ca5E81d10EFA8ACdd68B0B67Eb4B0a9f6",
+            amount: "9",
+        },
+        {
+            action: "stake",
+            user: "0x609d344A04245104C312925D2F5aE04F643A10CB",
+            amount: "8",
+        },
+        {
+            action: "stake",
+            user: "0x7019943Ca5E81d10EFA8ACdd68B0B67Eb4B0a9f7",
+            amount: "6",
+        },
+        {
+            action: "stake",
+            user: "0x609d344A04245104C312925D2F5aE04F643A10CB",
+            amount: "4",
+        },
+        {
+            action: "stake",
+            user: "0x7019943Ca5E81d10EFA8ACdd68B0B67Eb4B0a9f8",
+            amount: "2",
+        },
+        {
+            action: "stake",
+            user: "0x609d344A04245104C312925D2F5aE04F643A10CB",
+            amount: "9",
+        },
+        {
+            action: "stake",
+            user: "0x7019943Ca5E81d10EFA8ACdd68B0B67Eb4B0a9f9",
+            amount: "9",
+        },
+        {
+            action: "stake",
+            user: "0x609d344A04245104C312925D2F5aE04F643A10CB",
+            amount: "3",
+        },
+        {
+            action: "unstake",
+            user: "0x609d344A04245104C312925D2F5aE04F643A10CB",
+            amount: "5",
+        },
+        {
+            action: "unstake",
+            user: "0x7019943Ca5E81d10EFA8ACdd68B0B67Eb4B0a9f10",
+            amount: "8",
+        },
+        {
+            action: "unstake",
+            user: "0x609d344A04245104C312925D2F5aE04F643A10CB",
+            amount: "2",
+        },
+        {
+            action: "unstake",
+            user: "0x7019943Ca5E81d10EFA8ACdd68B0B67Eb4B0a9f11",
+            amount: "3",
+        },
+        {
+            action: "unstake",
+            user: "0x609d344A04245104C312925D2F5aE04F643A10CB",
+            amount: "2",
+        },
+        {
+            action: "unstake",
+            user: "0x609d344A04245104C312925D2F5aE04F643A10CB",
+            amount: "7",
+        },
+        {
+            action: "stake",
+            user: "0x7019943Ca5E81d10EFA8ACdd68B0B67Eb4B0a9f11",
+            amount: "8",
+        },
+        {
+            action: "stake",
+            user: "0x609d344A04245104C312925D2F5aE04F643A10CB",
+            amount: "10",
+        },
+        {
+            action: "stake",
+            user: "0x7019943Ca5E81d10EFA8ACdd68B0B67Eb4B0a9f11",
+            amount: "6",
+        },
+        {
+            action: "stake",
+            user: "0x7019943Ca5E81d10EFA8ACdd68B0B67Eb4B0a9f12",
+            amount: "3",
+        },
+        {
+            action: "stake",
+            user: "0x609d344A04245104C312925D2F5aE04F643A10CB",
+            amount: "1",
+        },
+        {
+            action: "stake",
+            user: "0x609d344A04245104C312925D2F5aE04F643A10CB",
+            amount: "6",
+        },
+        {
+            action: "withdrawUTokens",
+            user: "0x7019943Ca5E81d10EFA8ACdd68B0B67Eb4B0a9f12",
+            amount: "4",
+            cosmosAddr: "cosmos1t2ya2xu69xlkgkktfp94q0agclql8uvr44ezzj",
+        },
+        {
+            action: "withdrawUTokens",
+            user: "0x609d344A04245104C312925D2F5aE04F643A10CB",
+            amount: "6",
+            cosmosAddr: "cosmos1edsdz6c5mdg0gxd7c6xwpa4mlvqkm00r3effhu",
+        },
+        {
+            action: "withdrawUTokens",
+            user: "0x7019943Ca5E81d10EFA8ACdd68B0B67Eb4B0a9f13",
+            amount: "2",
+            cosmosAddr: "cosmos1t2ya2xu69xlkgkktfp94q0agclql8uvr44ezzj",
+        },
+        {
+            action: "withdrawUTokens",
+            user: "0x609d344A04245104C312925D2F5aE04F643A10CB",
+            amount: "7",
+            cosmosAddr: "cosmos1edsdz6c5mdg0gxd7c6xwpa4mlvqkm00r3effhu",
+        },
+        {
+            action: "withdrawUTokens",
+            user: "0x609d344A04245104C312925D2F5aE04F643A10CB",
+            amount: "9",
+            cosmosAddr: "cosmos1t2ya2xu69xlkgkktfp94q0agclql8uvr44ezzj",
+        },
+        {
+            action: "withdrawUTokens",
+            user: "0x7019943Ca5E81d10EFA8ACdd68B0B67Eb4B0a9f13",
+            amount: "7",
+            cosmosAddr: "cosmos1edsdz6c5mdg0gxd7c6xwpa4mlvqkm00r3effhu",
+        },
+        {
+            action: "withdrawUTokens",
+            user: "0x609d344A04245104C312925D2F5aE04F643A10CB",
+            amount: "3",
+            cosmosAddr: "cosmos1t2ya2xu69xlkgkktfp94q0agclql8uvr44ezzj",
+        },
+        {
+            action: "withdrawUTokens",
+            user: "0x7019943Ca5E81d10EFA8ACdd68B0B67Eb4B0a9f14",
+            amount: "5",
+            cosmosAddr: "cosmos1edsdz6c5mdg0gxd7c6xwpa4mlvqkm00r3effhu",
+        },
+        {
+            action: "withdrawUTokens",
+            user: "0x609d344A04245104C312925D2F5aE04F643A10CB",
+            amount: "4",
+            cosmosAddr: "cosmos1t2ya2xu69xlkgkktfp94q0agclql8uvr44ezzj",
+        },
+        {
+            action: "withdrawUTokens",
+            user: "0x609d344A04245104C312925D2F5aE04F643A10CB",
+            amount: "7",
+            cosmosAddr: "cosmos1edsdz6c5mdg0gxd7c6xwpa4mlvqkm00r3effhu",
+        },
+        {
+            action: "calculateRewards",
+            user: "0x7019943Ca5E81d10EFA8ACdd68B0B67Eb4B0a9f10",
+        },
+        {
+            action: "calculateRewards",
+            user: "0x609d344A04245104C312925D2F5aE04F643A10CB",
+        },
+    ]
+}
