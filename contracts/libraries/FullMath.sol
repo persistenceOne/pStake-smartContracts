33af98e2f67fbc5d4e8810d5d6785b01a891f715
ciberexplosion <51422182+ciberexplosion@users.noreply.github.com>
2021-09-22 16:49:15 +0530

    Update FullMath.sol

--- contracts/libraries/FullMath.sol
+++ contracts/libraries/FullMath.sol
@@ -5,7 +5,7 @@ pragma solidity >= 0.7.0;
 // license is CC-BY-4.0
 library FullMath {
     function fullMul(uint256 x, uint256 y) internal pure returns (uint256 l, uint256 h) {
-		uint256 mm = mulmod(x, y, type(uint256).max);
+	uint256 mm = mulmod(x, y, type(uint256).max);
         l = x * y;
         h = mm - l;
         if (mm < l) h -= 1;
@@ -16,10 +16,10 @@ library FullMath {
         uint256 h,
         uint256 d
     ) private pure returns (uint256) {
-	    uint256 pow2 = d & (~d + 1);
+	uint256 pow2 = d & (~d + 1);
         d /= pow2;
         l /= pow2;
-		l += h * ((~pow2 + 1) / pow2 + 1);
+	l += h * ((~pow2 + 1) / pow2 + 1);
         uint256 r = 1;
         r *= 2 - d * r;
         r *= 2 - d * r;
