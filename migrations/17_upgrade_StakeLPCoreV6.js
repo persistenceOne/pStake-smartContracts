d72ec4eca389d407baa97002a1ec439c2a0a9ecc
kombos <18464450+kombos@users.noreply.github.com>
2021-09-23 17:07:26 +0530

    multiple upgrades of stakeLP

---
+++ migrations/17_upgrade_StakeLPCoreV6.js
@@ -0,0 +1,69 @@
+const StakeLPCoreArtifactV6 = artifacts.require("StakeLPCoreV6");
+const StakeLPCoreArtifactV5 = artifacts.require("StakeLPCoreV5");
+
+const { upgradeProxy } = require("@openzeppelin/truffle-upgrades");
+var StakeLPCoreInstance;
+
+module.exports = async function (deployer, network, accounts) {
+  if (network === "development") {
+    let gasPriceGanache = 3e10;
+    let gasLimitGanache = 800000;
+    await StakeLPCore(gasPriceGanache, gasLimitGanache, deployer, accounts);
+  }
+
+  if (network === "ropsten") {
+    let gasPriceRopsten = 1e11;
+    let gasLimitRopsten = 7000000;
+    await StakeLPCore(gasPriceRopsten, gasLimitRopsten, deployer, accounts);
+  }
+
+  if (network === "goerli") {
+    let gasPriceGoerli = 5e12;
+    let gasLimitGoerli = 4000000;
+    await StakeLPCore(gasPriceGoerli, gasLimitGoerli, deployer, accounts);
+  }
+
+  if (network === "mainnet") {
+    let gasPriceMainnet = 5e10;
+    let gasLimitMainnet = 7000000;
+    await StakeLPCore(gasPriceMainnet, gasLimitMainnet, deployer, accounts);
+  }
+};
+
+async function StakeLPCore(gasPrice, gasLimit, deployer, accounts) {
+  console.log(
+    "inside StakeLPCore(),",
+    " gasPrice: ",
+    gasPrice,
+    " gasLimit: ",
+    gasLimit,
+    " deployer: ",
+    deployer.network,
+    " accounts: ",
+    accounts
+  );
+
+  let from_defaultAdmin = accounts[0];
+  let batchingLimit = 20;
+
+  StakeLPCoreInstance = await upgradeProxy(
+    StakeLPCoreArtifactV5.address,
+    StakeLPCoreArtifactV6,
+    { deployer }
+  );
+
+  console.log("StakeLPCore upgraded to StakeLPCoreV6: ", StakeLPCoreInstance.address);
+
+  // set contract addresses in StakeLPCore Contract
+  /*  const txReceipt = await StakeLPCoreInstance.setBatchingLimit(
+    batchingLimit,
+    {
+      from: from_defaultAdmin,
+      gasPrice: gasPrice,
+      gas: gasLimit,
+    }
+  );
+  console.log("setBatchingLimit() done"); */
+
+  console.log("ALL DONE.");
+}
