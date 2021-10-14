cb90a115abca097a48667b52a4f8e0d949572002
aditim121 <aditimaheshwari121@gmail.com>
2021-10-14 14:06:44 +0530

    XPRT mainnet deployemnt with holder contract

---
+++ contracts/holder/HolderSushiswap_STKXPRT_ETH.sol
@@ -0,0 +1,80 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity >=0.7.0;
+
+import "../interfaces/IHolder.sol";
+import "../interfaces/ISTokensXPRT.sol";
+import "@openzeppelin/contracts-upgradeable/access/AccessControlUpgradeable.sol";
+import "@openzeppelin/contracts-upgradeable/utils/PausableUpgradeable.sol";
+import "@openzeppelin/contracts-upgradeable/proxy/Initializable.sol";
+import "../libraries/TransferHelper.sol";
+
+contract HolderSushiswap_STKXPRT_ETH is
+IHolder,
+Initializable,
+AccessControlUpgradeable,
+PausableUpgradeable
+{
+	// constant pertaining to access roles
+	bytes32 public constant PAUSER_ROLE = keccak256("PAUSER_ROLE");
+
+	// constant pertaining to access roles
+	bytes32 public constant ACCOUNTANT_ROLE = keccak256("ACCOUNTANT_ROLE");
+
+	// value divisor to make weight factor a fraction if need be
+	uint256 public _valueDivisor;
+
+	// variable pertaining to contract upgrades versioning
+	uint256 public _version;
+
+	/**
+	 * @dev Constructor for initializing the Holder Uniswap contract.
+	 */
+	function initialize(
+		address pauserAdmin,
+		address accountantAdmin,
+		uint256 valueDivisor
+	) public virtual initializer {
+		__AccessControl_init();
+		_setupRole(PAUSER_ROLE, pauserAdmin);
+		_setupRole(ACCOUNTANT_ROLE, accountantAdmin);
+		_setupRole(DEFAULT_ADMIN_ROLE, _msgSender());
+		_valueDivisor = valueDivisor;
+		_version = 1;
+	}
+
+	/**
+	 * @dev get SToken reserve supply of the whitelisted contract
+	 * argument names commented to suppress warnings
+	 */
+	function getSTokenSupply(address whitelistedAddress, address sTokenAddress)
+	public
+	view
+	virtual
+	override
+	returns (uint256 sTokenSupply)
+	{
+		sTokenSupply = ISTokensXPRT(sTokenAddress).balanceOf(whitelistedAddress);
+		return sTokenSupply;
+	}
+
+	function safeTransfer(
+		address token,
+		address to,
+		uint256 value
+	) public virtual override {
+		require(hasRole(ACCOUNTANT_ROLE, _msgSender()), "HU3");
+		// finally transfer the new LP Tokens to the user address
+		TransferHelper.safeTransfer(token, to, value);
+	}
+
+	function safeTransferFrom(
+		address token,
+		address from,
+		address to,
+		uint256 value
+	) public virtual override {
+		require(hasRole(ACCOUNTANT_ROLE, _msgSender()), "HU4");
+		// finally transfer the new LP Tokens to the user address
+		TransferHelper.safeTransferFrom(token, from, to, value);
+	}
+}
---
+++ migrations/6_deploy_HolderSushiswap.js
@@ -0,0 +1,103 @@
+const HolderSushiswapStkXPRTEthArtifact = artifacts.require(
+  "HolderSushiswap_STKXPRT_ETH"
+);
+var networkID;
+
+// const { BN } = web3.utils.BN;
+const { deployProxy } = require("@openzeppelin/truffle-upgrades");
+const { BN } = web3.utils.BN;
+var HolderSushiswapStkXPRTEthInstance;
+
+module.exports = async function (deployer, network, accounts) {
+  if (network === "development") {
+    let gasPriceGanache = 3e10;
+    let gasLimitGanache = 800000;
+    networkID = 5777;
+    await deployContract(gasPriceGanache, gasLimitGanache, deployer, accounts);
+  }
+
+  if (network === "ropsten") {
+    let gasPriceRopsten = 1e11;
+    let gasLimitRopsten = 5000000;
+    networkID = 3;
+    await deployContract(gasPriceRopsten, gasLimitRopsten, deployer, accounts);
+  }
+
+  if (network === "goerli") {
+    let gasPriceGoerli = 5e12;
+    let gasLimitGoerli = 4000000;
+    networkID = 5;
+    await deployContract(gasPriceGoerli, gasLimitGoerli, deployer, accounts);
+  }
+
+  if (network === "mainnet") {
+    let gasPriceMainnet = 1e11;
+    let gasLimitMainnet = 7000000;
+    networkID = 1;
+    await deployContract(gasPriceMainnet, gasLimitMainnet, deployer, accounts);
+  }
+};
+
+async function deployContract(gasPrice, gasLimit, deployer, accounts) {
+  console.log(
+    "inside deployContract(),",
+    " gasPrice: ",
+    gasPrice,
+    " gasLimit: ",
+    gasLimit,
+    " deployer: ",
+    deployer.network,
+    " accounts: ",
+    accounts
+  );
+  // init parameters
+  let pauseAdmin = accounts[0];
+  let from_defaultAdmin = accounts[0];
+  let valueDivisor = new BN("1000000000");
+
+  let StakeLPAddress = "0xA1cF35f35031c0f2a27283793bE23a22fd615F58";
+  let WhitelistedRewardEmissionAddress = "0xdDa26973bB8a53BCb0b20a76Edf47474945784F2";
+
+  // let WhitelistedDivisor = new BN("1000000000");
+
+  HolderSushiswapStkXPRTEthInstance = await deployProxy(
+    HolderSushiswapStkXPRTEthArtifact,
+    [pauseAdmin, from_defaultAdmin, valueDivisor],
+    { deployer, initializer: "initialize" }
+  );
+  console.log(
+    "HolderSushiswapStkXPRTEth deployed: ",
+    HolderSushiswapStkXPRTEthInstance.address
+  );
+
+  const txReceiptGrantRole1 =
+    await HolderSushiswapStkXPRTEthInstance.grantRole(
+      "0x369da55721ba2b3acddd63aac7d6512c3e5762a78fa01c44f423f97868330c34", StakeLPAddress,
+      {
+        from: from_defaultAdmin,
+        gasPrice: gasPrice,
+        gas: gasLimit,
+      });
+  console.log("grantRole() set for HolderSushiswapStkXPRTEth contract.");
+
+  const txReceiptGrantRole2 =
+    await HolderSushiswapStkXPRTEthInstance.grantRole(
+      "0x369da55721ba2b3acddd63aac7d6512c3e5762a78fa01c44f423f97868330c34", WhitelistedRewardEmissionAddress,
+      {
+        from: from_defaultAdmin,
+        gasPrice: gasPrice,
+        gas: gasLimit,
+      });
+  console.log("grantRole() set for HolderSushiswapStkXPRTEth contract");
+
+  // set contract addresses in UTokens Contract
+  /* const txReceiptSetHolderSushiswapStkXPRTEthContract =
+    await PstakeInstance.setHolderSushiswapStkXPRTEthContract(StakeLPInstance.address, {
+      from: from_defaultAdmin,
+      gasPrice: gasPrice,
+      gas: gasLimit,
+    });
+  console.log("setHolderSushiswapStkXPRTEthContract() set for StakeLP contract."); */
+
+  console.log("ALL DONE.");
+}
