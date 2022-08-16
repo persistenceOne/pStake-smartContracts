aeecdb7bcd817519536d2d070ee671d17953732a
Aditi Maheshwari <aditimaheshwari121@gmail.com>
2022-08-16 13:18:16 +0400

    Migration upgrade

---
+++ contracts/interfaces/IMigrationAdminV2.sol
@@ -0,0 +1,100 @@
+/*
+ Copyright [2019] - [2021], PERSISTENCE TECHNOLOGIES PTE. LTD. and the pStake-smartContracts contributors
+ SPDX-License-Identifier: Apache-2.0
+*/
+
+pragma solidity >=0.7.0;
+
+/**
+ * @dev Interface of the ITokenWrapper.
+ */
+interface IMigrationAdminV2 {
+
+    /**
+     * @dev Set UTokens smart contract.
+	 * Emits a {SetUTokensContract} event.
+	 */
+    function setUTokensContract(address uAddress) external;
+
+    /**
+     * @dev Emitted when contract addresses are set
+	 */
+    event SetUTokensContract(address indexed _contract);
+
+    /**
+     * @dev Set STokens smart contract.
+	 * Emits a {SetSTokensContract} event.
+	 */
+    function setSTokensContract(address sAddress) external;
+
+    /**
+     * @dev Emitted when contract addresses are set
+	 */
+    event SetSTokensContract(address indexed _contract);
+
+    /**
+     * @dev Set Token Wrapper smart contract.
+	 * Emits a {SetTokenWrapperContract} event.
+	 */
+    function setTokenWrapperContract(address tokenWrapper) external;
+
+    /**
+     * @dev Emitted when contract addresses are set
+	 */
+    event SetTokenWrapperContract(address indexed _contract);
+
+    /**
+     * @dev Call SToken, TokenWrapper, LiquidStaking contract function.
+	 * Emits a {ClaimPendingRewardsEvent} event.
+	 * Emits a {ClaimUnbondedRewardsEvent} event.
+	 * Emits a {BurnSTokensEvent} event.
+	 */
+    function Migrate(address accountAddress, string memory toChainAddress) external returns (bool success);
+
+    /**
+     * @dev Emitted when contract addresses are set
+	 */
+    event ClaimPendingRewardsEvent(address indexed accountAddress);
+
+    /**
+     * @dev Emitted when contract addresses are set
+	 */
+    event ClaimUnbondedRewardsEvent(address indexed accountAddress);
+
+    /**
+     * @dev Emitted when contract addresses are set
+	 */
+    event WithdrawUTokensEvent(address indexed accountAddress, uint256 currentUTokenBalance, string toChainAddress);
+
+    /**
+     * @dev Emitted when contract addresses are set
+	 */
+    event BurnSTokensEvent(address indexed accountAddress, uint256 currentSTokenBalance);
+
+    /**
+     * @dev Triggers stopped state.
+	 *
+	 * Requirements:
+	 *
+	 * - The contract must not be paused.
+	 */
+    function pause() external returns (bool success);
+
+    /**
+     * @dev Returns to normal state.
+	 *
+	 * Requirements:
+	 *
+	 * - The contract must be paused.
+	 */
+    function unpause() external returns (bool success);
+
+    /**
+     * @dev Add hrp bytes
+	 *
+	 * Requirements:
+	 *
+	 * Can be called by admin only
+	 */
+    function setHRPBytes(bytes memory hrpBytes) external returns (bool success);
+}
---
+++ migrations/27_upgrade_MigrationAdminV2.js
@@ -0,0 +1,93 @@
+/*
+ Copyright [2019] - [2021], PERSISTENCE TECHNOLOGIES PTE. LTD. and the pStake-smartContracts contributors
+ SPDX-License-Identifier: Apache-2.0
+*/
+
+const MigrationAdminArtifactV2 = artifacts.require("MigrationAdminV2");
+const MigrationAdminArtifact = artifacts.require("MigrationAdmin");
+
+const { upgradeProxy } = require("@openzeppelin/truffle-upgrades");
+var MigrationAdminInstance;
+
+module.exports = async function (deployer, network, accounts) {
+    if (network === "development") {
+        let gasPriceGanache = 3e10;
+        let gasLimitGanache = 800000;
+        await upgradeMigrationAdmin(
+            gasPriceGanache,
+            gasLimitGanache,
+            deployer,
+            accounts
+        );
+    }
+
+    if (network === "ropsten") {
+        let gasPriceRopsten = 3e10;
+        let gasLimitRopsten = 7000000;
+        await upgradeMigrationAdmin(
+            gasPriceRopsten,
+            gasLimitRopsten,
+            deployer,
+            accounts
+        );
+    }
+
+    if (network === "goerli") {
+        let gasPriceGoerli = 5e12;
+        let gasLimitGoerli = 4000000;
+        await upgradeMigrationAdmin(
+            gasPriceGoerli,
+            gasLimitGoerli,
+            deployer,
+            accounts
+        );
+    }
+
+    if (network === "mainnet") {
+        let gasPriceMainnet = 5e10;
+        let gasLimitMainnet = 7000000;
+        await upgradeMigrationAdmin(
+            gasPriceMainnet,
+            gasLimitMainnet,
+            deployer,
+            accounts
+        );
+    }
+};
+
+async function upgradeMigrationAdmin(gasPrice, gasLimit, deployer, accounts) {
+    console.log(
+        "inside upgradeMigrationAdmin(),",
+        " gasPrice: ",
+        gasPrice,
+        " gasLimit: ",
+        gasLimit,
+        " deployer: ",
+        deployer.network,
+        " accounts: ",
+        accounts
+    );
+
+    let from_defaultAdmin = accounts[0];
+
+    MigrationAdminInstance = await upgradeProxy(
+        MigrationAdminArtifact.address,
+        MigrationAdminArtifactV2,
+        { deployer }
+    );
+
+    console.log("MigrationAdminV2 upgraded: ", MigrationAdminInstance.address);
+
+    /*// set contract addresses in LiquidStaking Contract
+    const txReceipt = await MigrationAdminInstance.setHRPBytes(
+        "persistence",
+        {
+            from: from_defaultAdmin,
+            gasPrice: gasPrice,
+            gas: gasLimit,
+        }
+    );
+    console.log("setHRPBytes() done");*/
+
+    console.log("ALL DONE.");
+}
