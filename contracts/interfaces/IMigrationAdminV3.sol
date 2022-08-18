68fddaff73e4c947128993eb99d540b8673a495d
Aditi Maheshwari <aditimaheshwari121@gmail.com>
2022-08-18 14:23:51 +0400

    migration

---
+++ contracts/interfaces/IMigrationAdminV3.sol
@@ -0,0 +1,114 @@
+/*
+ Copyright [2019] - [2021], PERSISTENCE TECHNOLOGIES PTE. LTD. and the pStake-smartContracts contributors
+ SPDX-License-Identifier: Apache-2.0
+*/
+
+pragma solidity >=0.7.0;
+
+/**
+ * @dev Interface of the ITokenWrapper.
+ */
+interface IMigrationAdminV2 {
+
+    /**
+     * @dev Set UTokens smart contract.
+	 * Emits a {SetUTokensContract} event.
+	 */
+    function setUTokensContract(address uAddress) external;
+
+    /**
+     * @dev Emitted when contract addresses are set
+	 */
+    event SetUTokensContract(address indexed _contract);
+
+    /**
+     * @dev Set STokens smart contract.
+	 * Emits a {SetSTokensContract} event.
+	 */
+    function setSTokensContract(address sAddress) external;
+
+    /**
+     * @dev Emitted when contract addresses are set
+	 */
+    event SetSTokensContract(address indexed _contract);
+
+    /**
+     * @dev Set Token Wrapper smart contract.
+	 * Emits a {SetTokenWrapperContract} event.
+	 */
+    function setTokenWrapperContract(address tokenWrapper) external;
+
+    /**
+     * @dev Emitted when contract addresses are set
+	 */
+    event SetTokenWrapperContract(address indexed _contract);
+
+    /**
+     * @dev Call SToken, TokenWrapper, LiquidStaking contract function.
+	 * Emits a {ClaimPendingRewardsEvent} event.
+	 * Emits a {ClaimUnbondedRewardsEvent} event.
+	 * Emits a {BurnSTokensEvent} event.
+	 */
+    function Migrate(address accountAddress, string memory toChainAddress, string memory toCosmosChainAddress) external returns (bool success);
+
+    /**
+     * @dev Emitted when contract addresses are set
+	 */
+    event ClaimPendingRewardsEvent(address indexed accountAddress);
+
+    /**
+     * @dev Emitted when contract addresses are set
+	 */
+    event ClaimUnbondedRewardsEvent(address indexed accountAddress);
+
+    /**
+     * @dev Emitted when contract addresses are set
+	 */
+    event WithdrawUTokensEvent(address indexed accountAddress, uint256 currentUTokenBalance, string toCosmosChainAddress);
+
+    /**
+     * @dev Emitted when contract addresses are set
+	 */
+    event BurnSTokensEvent(address indexed accountAddress, uint256 currentSTokenBalance, string toPersistenceChainAddress);
+
+    /**
+     * @dev Emitted when contract addresses are set
+	 */
+    event SetMigrationCompleteEvent(address indexed accountAddress, uint256 currentSTokenBalance, uint256 currentUTokenBalance, string toCosmosChainAddress, string toPersistenceChainAddress);
+
+    /**
+     * @dev Triggers stopped state.
+	 *
+	 * Requirements:
+	 *
+	 * - The contract must not be paused.
+	 */
+    function pause() external returns (bool success);
+
+    /**
+     * @dev Returns to normal state.
+	 *
+	 * Requirements:
+	 *
+	 * - The contract must be paused.
+	 */
+    function unpause() external returns (bool success);
+
+    /**
+     * @dev Add hrp bytes
+	 *
+	 * Requirements:
+	 *
+	 * Can be called by admin only
+	 */
+    function setHRPBytes(bytes memory hrpBytes) external returns (bool success);
+
+    /**
+     * @dev Add hrp bytes
+	 *
+	 * Requirements:
+	 *
+	 * Can be called by admin only
+	 */
+    function setCosmosHRPBytes(bytes memory hrpBytes) external returns (bool success);
+}
